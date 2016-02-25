6ecd2033027f9ceba2b70198bcdf8d03cf254bf9
Natalie <natalie.s.sharpe@gmail.com>
2016-02-25 13:48:14 -0600

    closes #55

--- client/app/match/match.js
+++ client/app/match/match.js
@@ -5,6 +5,14 @@ angular.module('cinePlate.match', [])
 .controller('MatchCtrl', ['$scope', '$http', '$routeParams', 'Matches', function($scope, $http, $routeParams, Matches) {
   $scope.contentLoaded = false;
 
+  //different types to populate the dropdown menu
+  $scope.pages = ['My Profile', 'Home', 'Sign In', 'Sign Up', 'Sign out'];
+  $scope.clicked = false;
+  //create an empty object to store form data
+  $scope.userChoice = {
+    page: ''
+  };
+
   $scope.stars = [1, 2, 3, 4, 5];
   $scope.movie = { 
     title: 'Kung Fu Panda',
